c40b2479329284ca4744473a209f8e4a201baaf4
Sonny-Mooore <110048965+Sonny-Mooore@users.noreply.github.com>
2024-07-30 04:57:55 +0300

    styles

---
+++ mushmula/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react';
+import styled from 'styled-components';
+
+
+const Root = styled.div`
+  height: 160px;
+  border: black solid 1px;
+
+`;
+
+
+export const Header: FC = () => {
+  return (
+    <Root>
+      sdkjf
+    </Root>
+  );
+};
--- mushmula/components/Main/Main.tsx
+++ mushmula/components/Main/Main.tsx
@@ -3,5 +3,7 @@ import styled from "styled-components";
 
 export const Main = styled.main`
   max-width: 1920px;
+  margin: 0 auto;
+  height: 100vh;
   border: red solid 1px;
 `
