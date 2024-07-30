91185f4a36cdc7d0b58596803b92c0d7a915c142
Sonny-Mooore <110048965+Sonny-Mooore@users.noreply.github.com>
2024-07-30 04:40:41 +0300

    prettier

--- mushmula/pages/_app.tsx
+++ mushmula/pages/_app.tsx
@@ -1,27 +1,30 @@
-import React, {FC} from 'react'
-import {AppProps} from 'next/app'
-import isPropValid from "@emotion/is-prop-valid";
-import {StyleSheetManager} from "styled-components";
-import Head from "next/head";
+import React, { FC } from 'react';
+import { AppProps } from 'next/app';
+import isPropValid from '@emotion/is-prop-valid';
+import { StyleSheetManager } from 'styled-components';
+import Head from 'next/head';
 import './globals.css';
+import { Main } from '../components/Main/Main';
 
 
-const MyApp: FC<AppProps> = ({Component, pageProps}) => {
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
 
-  console.log("Sadfd")
+  console.log('Sadfd');
   return (
     <StyleSheetManager shouldForwardProp={prop => isPropValid(prop)}>
       <Head>
-        <meta charSet="UTF-8"/>
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link rel="shortcut icon" href="/images/favicon.png"/>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="shortcut icon" href="/images/favicon.png" />
         <title>mushmula</title>
       </Head>
-      <Component {...pageProps} />
+      <Main>
+        <Component {...pageProps} />
+      </Main>
     </StyleSheetManager>
 
-  )
-}
+  );
+};
 
-export default MyApp
+export default MyApp;
